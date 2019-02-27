2c681c54976751428ff2e92991a6f8ab3ead2218
Sparking <sparking02@yahoo.com.mx>
2019-02-26 22:36:34 -0600

    Exciting, next chapter https://discoverthreejs.com/book/1-first-steps/5-camera-controls/

--- ThreeJS/Ch1.1/js/app.js
+++ ThreeJS/Ch1.1/js/app.js
@@ -1,44 +1,71 @@
 // Just waiting for your beautiful creation
 
-const container = document.querySelector('#container');
+let container;
+let camera;
+let renderer;
+let scene;
+let mesh;
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('skyblue');
-
-const fov = 35;
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1;
-const far = 100;
-
-const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
-camera.position.set(0, 0, 10);
-
-const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
-const material = new THREE.MeshStandardMaterial({color: 0x800080});
-const mesh = new THREE.Mesh(geometry, material);
-
-scene.add(mesh);
-
-const light = new THREE.DirectionalLight(0xffffff,5.0);
-light.position.set(0,3,3);
-scene.add(light);
-
-renderer = new THREE.WebGLRenderer({antialias: true});
-
-//const renderer = new THREE.WebGLRenderer();
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-container.appendChild(renderer.domElement);
-
-function animate()
+function init()
 {
-    requestAnimationFrame(animate);
+    container = document.querySelector('#container');
+    
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x8FBCD4);
 
+    const fov = 35;
+    const aspect = container.clientWidth / container.clientHeight;
+    const near = 0.1;
+    const far = 100;
+    
+    camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
+    camera.position.set(0, 0, 10);
+
+    const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load('textures/uv_test_bw.png');
+    texture.anisotropy = 16;
+    const material = new THREE.MeshStandardMaterial({
+        map: texture,
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    
+    scene.add(mesh);
+    
+    const light = new THREE.DirectionalLight(0xffffff,3.0);
+    light.position.set(0,3,3);
+    scene.add(light);
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.appendChild(renderer.domElement);
+    renderer.setAnimationLoop( () => {
+        update();
+        render();    
+    });
+}
+
+function update()
+{
     mesh.rotation.z += 0.01;
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.01;
+}
 
+function render()
+{
     renderer.render(scene,camera);
 }
 
-animate();
+function onWindowResize()
+{
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
+init();
