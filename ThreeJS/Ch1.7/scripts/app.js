283e0e6e3d32c8a8e759475949ebe7f84873c61a
Sparking <sparking02@yahoo.com.mx>
2019-03-22 19:35:56 -0600

    Free part of the book completed

--- ThreeJS/Ch1.7/scripts/app.js
+++ ThreeJS/Ch1.7/scripts/app.js
@@ -4,6 +4,9 @@ let controls;
 let renderer;
 let scene;
 
+const mixers = [];
+const clock = new THREE.Clock();
+
 function init() {
 
   container = document.querySelector( '#scene-container' );
@@ -14,7 +17,7 @@ function init() {
   createCamera();
   createControls();
   createLights();
-  createMeshes();
+  loadModels();
   createRenderer();
 
   renderer.setAnimationLoop( () => {
@@ -29,7 +32,7 @@ function init() {
 function createCamera() {
 
   camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100 );
-  camera.position.set( -5, 5, 7 );
+  camera.position.set( -1.5, 1.5, 6.5 );
 
 }
 
@@ -50,97 +53,39 @@ function createLights() {
 
 }
 
-function createMaterials() {
+function loadModels(){
 
-  const body = new THREE.MeshStandardMaterial( {
-    color: 0xff3333, // red
-    flatShading: true,
-  } );
+  const loader = new THREE.GLTFLoader();
 
-  // just as with textures, we need to put colors into linear color space
-  body.color.convertSRGBToLinear();
+  const onLoad = ( gltf, position ) => {
 
-  const detail = new THREE.MeshStandardMaterial( {
-    color: 0x333333, // darkgrey
-    flatShading: true,
-  } );
-
-  detail.color.convertSRGBToLinear();
-
-  return {
-
-    body,
-    detail,
-
+    const model = gltf.scene.children[ 0 ];
+    model.position.copy( position );
+  
+    const animation = gltf.animations[0];
+  
+    const mixer = new THREE.AnimationMixer(model);
+    mixers.push(mixer);
+  
+    const action = mixer.clipAction(animation);
+    action.play();
+  
+    scene.add(model);
   };
 
-}
+  const onProgress = () =>{};
 
-function createGeometries() {
+  const onError = (errorMessage) => {console.log(errorMessage);};
 
-  const nose = new THREE.CylinderBufferGeometry( 0.75, 0.75, 3, 12 );
+  const parrotPosition = new THREE.Vector3(0, 0, 2.5);
+  loader.load('models/Parrot.glb',gltf => onLoad(gltf,parrotPosition),onProgress,onError);
 
-  const cabin = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
+  const flamingoPosition = new THREE.Vector3( 7.5, 0, -10 );
+  loader.load( 'models/Flamingo.glb', gltf => onLoad( gltf, flamingoPosition ), onProgress, onError );
 
-  const chimney = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
+  const storkPosition = new THREE.Vector3( 0, -2.5, -10 );
+  loader.load( 'models/Stork.glb', gltf => onLoad( gltf, storkPosition ), onProgress, onError );
 
-  // we can reuse a single cylinder geometry for all 4 wheels
-  const wheel = new THREE.CylinderBufferGeometry( 0.4, 0.4, 1.75, 16 );
-  wheel.rotateX( Math.PI / 2 );
-
-  return {
-    nose,
-    cabin,
-    chimney,
-    wheel,
-  };
-
-}
-
-function createMeshes() {
-
-  // create a Group to hold the pieces of the train
-  const train = new THREE.Group();
-  scene.add( train );
-
-  const materials = createMaterials();
-  const geometries = createGeometries();
-
-  const nose = new THREE.Mesh( geometries.nose, materials.body );
-  nose.rotation.z = Math.PI / 2;
-  nose.position.x = -1;
-
-  const cabin = new THREE.Mesh( geometries.cabin, materials.body );
-  cabin.position.set( 1.5, 0.4, 0 );
-
-  const chimney = new THREE.Mesh( geometries.chimney, materials.detail );
-  chimney.position.set( -2, 0.9, 0 );
-
-  const smallWheelRear = new THREE.Mesh( geometries.wheel, materials.detail );
-  smallWheelRear.position.set( 0, -0.5, 0 );
-
-  const smallWheelCenter = smallWheelRear.clone();
-  smallWheelCenter.position.x = -1;
-
-  const smallWheelFront = smallWheelRear.clone();
-  smallWheelFront.position.x = -2;
-
-  const bigWheel = smallWheelRear.clone();
-  bigWheel.scale.set( 2, 2, 1.25 );
-  bigWheel.position.set( 1.5, -0.1, 0 );
-
-  train.add(
-
-    nose,
-    cabin,
-    chimney,
-
-    smallWheelRear,
-    smallWheelCenter,
-    smallWheelFront,
-    bigWheel,
-
-  );
 
 }
 
@@ -160,7 +105,11 @@ function createRenderer() {
 
 }
 
-function update() {}
+function update() {
+  const delta = clock.getDelta();
+  
+  mixers.forEach((mixer) => {mixer.update(delta);});
+}
 
 function render() {
 
